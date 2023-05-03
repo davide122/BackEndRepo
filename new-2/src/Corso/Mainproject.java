d85a1b000734c3ae5a6f2f94c3b71af5afc19100
davide122 <davidemarchica277@gmail.com>
2023-05-03 09:32:09 +0200

    ciaaociao

---
+++ new-2/src/Corso/Mainproject.java
@@ -0,0 +1,25 @@
+package Corso;
+
+public class Mainproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean b = false;
+		boolean a = true;
+		
+		char c = 'f';
+		System.out.println(c+"ciao");
+		
+		int o = 1;
+		System.out.println(o+"ciao");
+		
+		double s = 1.3;
+		String h = "ciao";
+
+		
+		float fs = 100.20f;
+		
+		
+	}
+
+}
