cf415c1cccee17bb998f839e635af4e551108cfa
davide122 <123973018+davide122@users.noreply.github.com>
2023-05-04 17:03:04 +0200

    Add files via upload
    nuovo esercizio

---
+++ ES3/src/IntLettere.java
@@ -0,0 +1,30 @@
+
+public class IntLettere {
+int lettere;
+public IntLettere(int lettere) {
+	this.lettere = lettere;
+
+	if (((lettere<=3)&&(lettere>=0))) {
+		int input = lettere;
+		switch (input) {
+		case 0:
+			System.out.println("zero");
+			break;
+		case 1:
+			System.out.println("uno");
+			break;
+		case 2:
+			System.out.println("due");
+			break;
+		case 3:
+			System.out.println("tre");
+			break;
+		}
+	}
+	else {
+		System.out.println("Mi dispiace, posso convertire numeri da 0 a 3");
+	}
+}
+
+
+}
---
+++ ES3/src/Paridispari.java
@@ -0,0 +1,24 @@
+
+public class Paridispari {
+String paridispari;
+
+
+
+public Paridispari(String paridispari) {
+	this.paridispari=paridispari;
+	if((paridispari.length()%2)==0) {
+		System.out.println(true);
+		return;
+	}else {
+		System.out.println(false);
+		return;
+	}
+}
+
+
+
+
+
+
+
+}
