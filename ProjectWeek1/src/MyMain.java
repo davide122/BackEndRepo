13acb9babe35e01ff33d4a0f7aa5dee0f64d043d
davide122 <123973018+davide122@users.noreply.github.com>
2023-05-06 18:18:17 +0200

    Add files via upload
    aggiornamento

--- ProjectWeek1/src/MyMain.java
+++ ProjectWeek1/src/MyMain.java
@@ -3,7 +3,7 @@ public class MyMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Theplayer b = new Theplayer();
+		Theplayer b = new Theplayer(false, 0, 0, null,0,false);
 	}
 
 }
